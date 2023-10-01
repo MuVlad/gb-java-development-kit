dae87951d32a82f028806e3c199634454b35a683
Vlad Muslimov <muslimov.vlad@mail.ru>
2023-10-01 17:29:35 +0500

    Доработанный чат согласно условиям домашнего задания

--- src/main/java/mu/vlad/chat/ChatWindow.java
+++ src/main/java/mu/vlad/chat/ChatWindow.java
@@ -6,15 +6,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
 
 public class ChatWindow extends JFrame {
-    static private final int WINDOW_HEIGHT = 640;
-    static private final int WINDOW_WIDTH = 720;
-    static private final int WINDOW_POS_X = 300;
-    static private final int WINDOW_POS_Y = 100;
-    static private final String WINDOW_NAME = "Чат с сервером";
+    private static final int WINDOW_HEIGHT = 640;
+    private static final int WINDOW_WIDTH = 720;
+    private static final int WINDOW_POS_X = 300;
+    private static final int WINDOW_POS_Y = 100;
+    private static final String WINDOW_NAME = "Чат с сервером";
 
 
     JTextArea textOutput = new JTextArea("");
@@ -31,6 +33,7 @@ public class ChatWindow extends JFrame {
         setBounds(WINDOW_POS_X, WINDOW_POS_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
 
         textOutput.setEditable(false);
+        textOutput.append(readingLogFile());
         grid.add(textOutput);
         grid.add(label);
         grid.add(textInput);
@@ -39,11 +42,48 @@ public class ChatWindow extends JFrame {
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textOutput.append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss/dd.MM")) + " " + login + " : " + textInput.getText() + "\n");
+                sendMsg(login);
             }
         });
 
+        textInput.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    sendMsg(login);
+                }
+            }
+        });
+
+
         add(grid);
         setVisible(true);
+    }
+
+    private void sendMsg(String login) {
+        String msg = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss/dd.MM")) + " " + login + " : " + textInput.getText() + "\n";
+        textOutput.append(msg);
+        WriteLogToFile(msg);
+        textInput.setText("");
+    }
+
+    private static void WriteLogToFile(String msg) {
+        try (BufferedWriter writerLog = new BufferedWriter(new FileWriter("log.txt", true))) {
+            writerLog.write(msg);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String readingLogFile() {
+        File fileLog = new File("log.txt");
+        if (fileLog.exists()) {
+            try (BufferedReader readerLogFile = new BufferedReader(new FileReader(fileLog))) {
+                return readerLogFile.lines().collect(Collectors.joining("\n"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return "";
     }
 }
