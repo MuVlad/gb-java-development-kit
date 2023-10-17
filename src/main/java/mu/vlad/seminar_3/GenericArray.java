f9fa4932032a94c3a96c3426931efc10e0c5f18f
Vlad Muslimov <muslimov.vlad@mail.ru>
2023-10-18 00:15:50 +0500

    hw 3 task 1 added corrections

--- src/main/java/mu/vlad/seminar_3/GenericArray.java
+++ src/main/java/mu/vlad/seminar_3/GenericArray.java
@@ -67,7 +67,7 @@ public class GenericArray<T> {
 
         @Override
         public Iterator<Object> iterator() {
-            return this;
+            return new GenericIterator();
         }
     }
 }
